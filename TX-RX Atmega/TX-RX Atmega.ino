ead5b94ea776a999d860c3fe1d30dbfc5540a169
MattyMar234 <mattymar2002@gmail.com>
2023-09-18 16:08:59 +0200

    Schema controller

--- TX-RX Atmega/TX-RX Atmega.ino
+++ TX-RX Atmega/TX-RX Atmega.ino
@@ -4,7 +4,7 @@
 #include <RF24_config.h>
 #include <SPI.h>
 
-#define DEBUG
+//#define DEBUG
 //#define AS_TRANSMITTER
 #define AS_RECEIVER
 
@@ -17,11 +17,13 @@
 #define CHANNEL 0x60
 #define CSN_PIN 10
 #define CE_PIN 8
+#define ERROR_LED_PIN 2
 
 
 
 RF24 radio(CE_PIN, CSN_PIN);  // using pin 7 for the CE pin, and pin 8 for the CSN pin
-
+long timerLED = 0;
+long timerPacket = 0;
 
 #ifdef AS_TRANSMITTER
   uint8_t READING_PIPE[] = {NODE1_ADDRESS}; //leggo dal canale del nodo 1
@@ -32,8 +34,9 @@ RF24 radio(CE_PIN, CSN_PIN);  // using pin 7 for the CE pin, and pin 8 for the C
   
 #endif
 
-uint8_t pins[] = {3,4,5,6};
+uint8_t pins[] = {3,4,5,6,7};
 boolean dataAvailable = false;
+boolean serialIsAvailable = false;
 
 
 typedef struct payloadPacket 
@@ -42,6 +45,7 @@ typedef struct payloadPacket
   uint8_t sw2;
   uint8_t sw3;
   uint8_t sw4;
+  uint8_t status;
 
 } Packet;
 
@@ -52,6 +56,8 @@ void setup()
 {
   SPI.begin();
   Serial.begin(115200);
+
+  serialIsAvailable = Serial;
 
   #ifdef AS_TRANSMITTER
     Serial.println(F("working as transmitter"));
@@ -66,6 +72,9 @@ void setup()
       pinMode(pins[i], OUTPUT);
       digitalWrite(pins[i], LOW);
     }
+
+    pinMode(ERROR_LED_PIN, OUTPUT);
+    digitalWrite(ERROR_LED_PIN, LOW);
   #endif
 
   
@@ -83,6 +92,8 @@ void setup()
     Serial.println((long)WRITING_PIPE, HEX);
   #else
     if (!radio.begin()) {
+      if(serialIsAvailable)
+        Serial.println(F("radio hardware is not responding!!"));
       while (1) {}  // hold in infinite loop
     }
   #endif
@@ -108,6 +119,8 @@ void setup()
   #else
     radio.startListening();   // put radio in RX mode
   #endif
+
+  packet.status = 0;
   
   
 
@@ -124,69 +137,93 @@ void loop()
 {
   #ifdef AS_TRANSMITTER
 
-    uint8_t *p = (uint8_t *) &packet;
-    for(int i = 0; i < (int)sizeof(pins)/sizeof(pins[0]); i++) 
-      p[i] = digitalRead(pins[i]);
-    
-    
-    unsigned long start_timer = micros();                // start the timer
-    bool report = radio.write(&packet, sizeof(packet));  // transmit & save the report
-    unsigned long end_timer = micros();
+    if(millis() - timerPacket >=  30) {
+      timerPacket = millis();
 
-    #ifdef DEBUG
-      if (report) {
-        Serial.print(F("Transmission successful! "));  // payload was delivered
-        Serial.print(F("Time to transmit = "));
-        Serial.print(end_timer - start_timer);        // print the timer result
-        Serial.print(F(" us. Sent: "));
+      uint8_t *p = (uint8_t *) &packet;
+      for(int i = 0; i < (int)sizeof(pins)/sizeof(pins[0]); i++) 
+        p[i] = digitalRead(pins[i]);
 
-        for(int i = 0; i < (int)sizeof(pins)/sizeof(pins[0]); i++) {
-          Serial.print(p[i]);
-          Serial.print(F(" "));
-        }
-        Serial.println();
-    } 
-    else {
-      Serial.println(F("Transmission failed or timed out"));  // payload was not delivered
-      Serial.print(F("Time to transmit = "));
-      Serial.print(end_timer - start_timer);        // print the timer result
-      Serial.print(F(" us."));
-      Serial.println();
-    }
-    #endif
-    delay(50);
-  #else
-    if(dataAvailable || true) {
-      uint8_t pipe;
-      dataAvailable = false;
-      // is there a payload? get the pipe number that recieved it
-      if (radio.available(&pipe)) 
-      {              
-        uint8_t *p = (uint8_t *) &packet;
-        uint8_t bytes = radio.getPayloadSize();  // get the size of the payload
-        radio.read(&packet, bytes);             // fetch payload from FIFO
+      if(millis() - timerLED >=  200) {
+        timerLED = millis();
 
-        for(int i = 0; i < (int)sizeof(pins)/sizeof(pins[0]); i++) {
-          digitalWrite(pins[i], p[i]);
-        }
+        if(packet.status == 0) 
+          packet.status = 1;
+        else
+          packet.status = 0;
+      }
+      
+      
+      unsigned long start_timer = micros();                // start the timer
+      bool report = radio.write(&packet, sizeof(packet));  // transmit & save the report
+      unsigned long end_timer = micros();
 
-        #ifdef DEBUG
-          Serial.print(F("Received "));
-          Serial.print(bytes);  // print the size of the payload
-          Serial.print(F(" bytes on pipe "));
-          Serial.print(pipe);  // print the pipe number
-          Serial.print(F(": "));
-        
+      #ifdef DEBUG
+        if (report) {
+          Serial.print(F("Transmission successful! "));  // payload was delivered
+          Serial.print(F("Time to transmit = "));
+          Serial.print(end_timer - start_timer);        // print the timer result
+          Serial.print(F(" us. Sent: "));
+
           for(int i = 0; i < (int)sizeof(pins)/sizeof(pins[0]); i++) {
             Serial.print(p[i]);
             Serial.print(F(" "));
           }
           Serial.println();
-        #endif
+      } 
+      else {
+        Serial.println(F("Transmission failed or timed out"));  // payload was not delivered
+        Serial.print(F("Time to transmit = "));
+        Serial.print(end_timer - start_timer);        // print the timer result
+        Serial.print(F(" us."));
+        Serial.println();
       }
+      #endif
     }
-  #endif
-}
+  #else
+
+    uint8_t pipe;
+    dataAvailable = false;
+    // is there a payload? get the pipe number that recieved it
+    if (radio.available(&pipe)) 
+    {              
+      timerPacket = millis(); //mi salvo il momento in cui ho ricevuto l'ultimo pacchetto
+      uint8_t *p = (uint8_t *) &packet;
+      uint8_t bytes = radio.getPayloadSize();  // get the size of the payload
+      radio.read(&packet, bytes);             // fetch payload from FIFO
+
+      for(int i = 0; i < (int)sizeof(pins)/sizeof(pins[0]); i++) {
+        digitalWrite(pins[i], p[i]);
+      }
+      digitalWrite(ERROR_LED_PIN, LOW);
+
+      #ifdef DEBUG
+        Serial.print(F("Received "));
+        Serial.print(bytes);  // print the size of the payload
+        Serial.print(F(" bytes on pipe "));
+        Serial.print(pipe);  // print the pipe number
+        Serial.print(F(": "));
+      
+        for(int i = 0; i < (int)sizeof(pins)/sizeof(pins[0]); i++) {
+          Serial.print(p[i]);
+          Serial.print(F(" "));
+        }
+        Serial.println();
+      #endif
+    }
+    //se è passato un secondo dall'umtimo pacchetto
+    //spengo tutto e attivo il LED di errore
+    else if(millis() - timerPacket >= 1000 )
+    {
+      for(int i = 0; i < (int)sizeof(pins)/sizeof(pins[0]); i++) {
+        digitalWrite(pins[i], LOW);
+      }
+      digitalWrite(ERROR_LED_PIN, HIGH);
+      delay(100);
+    }
+  }
+#endif
+
 
 #ifdef AS_RECEIVER
   void getPacketIRQ() {
